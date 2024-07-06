06585a37f2c8af4bd8a2cfb2ca08280110c305e2
Jozsef, Seres <jozsef.seres@evosoft.com>
2024-07-07 00:13:06 +0200

    add error handling

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { BookingService } from './booking/booking.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   providers: [BookingService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
@@ -15,12 +16,18 @@ export class AppComponent {
   constructor(private _bookSvc: BookingService) {
   }
 
+  public isError404: boolean = false;
+  public isError500: boolean = false;
+
   public form = new FormGroup({
     bookingRef: new FormControl('PZIGZ3', [Validators.required]),
     familyName: new FormControl('HESP', [Validators.required])
   })
 
   async onSubmit() {
+    this.isError404 = false;
+    this.isError500 = false;
+
     if (!this.form.valid) {
       alert('Error, form is invalid');
       return;
@@ -28,9 +35,18 @@ export class AppComponent {
 
     try {
       const result = await this._bookSvc.getBooking(this.form.value.bookingRef, this.form.value.familyName);
+
+      // todo: add a new route to display data
       alert('Booking found correctly');
     } catch (ex: any) {
-      alert(ex.message);
+      console.log(ex);
+      if (ex.status === 404) {
+        this.isError404 = true;
+      } else {
+        // this also contains network error, etc
+        // it might be worthwhile to display backend validation errors here
+        this.isError500 = true;
+      }
     }
   }
 }
